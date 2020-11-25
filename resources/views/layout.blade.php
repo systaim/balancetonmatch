6afba6bdd88f5ebeb050efb35333ce481d81f267
Anthony <systaim@gmail.com>
2020-11-25 19:10:41 +0100

    ajout logo club actuel

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -32,8 +32,8 @@
                     <div class="h-64 flex flex-col justify-end">
                         <div class="relative p-4 bg-darkGray rounded-lg text-white w-11/12 m-auto">
                             <div class="flex flex-row justify-center items-center px-4">
-                                <div class="h-24 w-24 bg-white rounded-full overflow-hidden absolute top--3 shadow-xl">
-                                    <img class="object-contain" src="{{ asset(Auth::user()->profile_photo_path) }}" alt="">
+                                <div class="logo h-24 w-24 bg-white rounded-full overflow-hidden absolute top--3 shadow-xl">
+                                    <img class="object-contain" src="https://android-apiapp.azureedge.net/common/bib_img/logo/{{ Auth::user()->club->numAffiliation }}.jpg" alt="">
                                 </div>
                                 <div class="overflow-hidden">
                                     <p class="font-bold capitalize text-lg mt-8 mb-4">{{ Auth::user()->first_name }} {{ Auth::user()->last_name }}</p>
