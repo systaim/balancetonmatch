309ecad52d05e4e7da5eccbad757abd619047827
Anthony <systaim@gmail.com>
2020-11-27 21:30:57 +0100

    module match responsive tablette

--- resources/views/match.blade.php
+++ resources/views/match.blade.php
@@ -6,9 +6,9 @@
     <a href="{{route('matches.show',$match)}}">
         <div class="grid grid-cols-12">
             <div class="col-span-5 overflow-hidden">
-                <div class="bg-primary p-2 text-secondary flex flex-col rounded-tl-lg">
+                <div class="bg-primary p-2 text-secondary flex flex-col rounded-l-lg sm:flex-row sm:items-center sm:rounded-l-full">
                     <div class="flex justify-center">
-                        <div class="logo h-8 w-8">
+                        <div class="logo h-8 w-8 sm:h-16 sm:w-16 cursor-pointer m-1">
                             <img class="object-contain" src="https://android-apiapp.azureedge.net/common/bib_img/logo/{{ $match->homeClub->numAffiliation }}.jpg" alt="logo">
                         </div>
                     </div>
@@ -36,9 +36,9 @@
                 @endif
             </div>
             <div class="col-span-5 overflow-hidden z-0">
-                <div class="bg-secondary p-2 text-primary flex flex-col rounded-tr-lg">
+                <div class="bg-secondary p-2 text-primary flex flex-col rounded-tr-lg sm:flex-row-reverse sm:items-center sm:rounded-r-full">
                     <div class="flex justify-center">
-                        <div class="logo h-8 w-8 cursor-pointer">
+                        <div class="logo h-8 w-8 sm:h-16 sm:w-16 cursor-pointer m-1">
                             <img class="object-contain" src="https://android-apiapp.azureedge.net/common/bib_img/logo/{{ $match->awayClub->numAffiliation }}.jpg" alt="logo">
                         </div>
                     </div>
