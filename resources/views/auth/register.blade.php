32f844a589d4db9ee4e9494f0d60c7543e1b2fe7
Anthony <systaim@gmail.com>
2020-11-30 18:17:46 +0100

    pages login et register modifiée

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -3,36 +3,37 @@
 <div class="bg-register py-4">
     <form method="POST" action="{{ route('register') }}">
         @csrf
-        <div class="m-4 p-6 sm:w-8/12 md:w-6/12 lg:w-5/12 bg-white rounded-lg shadow-xl">
-            <h2 class="text-primary text-center text-xl py-4">S'enregistrer</h2>
+        <div class="m-4 p-6 text-secondary rounded-lg shadow-xl bg-primary md:w-7/12 md:m-auto lg:w-8/12 xl:m-2">
+            <h2 class="text-center text-2xl py-4">S'enregistrer</h2>
             <form method="POST" action="{{ route('register') }}">
                 <div class="relative my-2 p-3">
                     <p class="text-sm mb-4">* champs obligatoire</p>
-                    <div class="xl:flex xl:justify-between mb-4">
-                        <div class="flex flex-col">
+                    
+                    <div class="lg:flex lg:justify-between">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="last_name">Mon nom de famille *</label>
-                            <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="text" name="last_name" id="last_name" :value="old('last_name')" required autocomplete="last_name">
+                            <input class="inputForm border-2 focus:outline-none focus:shadow-outline" type="text" name="last_name" id="last_name" :value="old('last_name')" required autocomplete="last_name">
                             @error('last_name')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="flex flex-col">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="first_name">Mon prénom *</label>
-                            <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="text" name="first_name" id="first_name" :value="old('first_name')" required autocomplete="first_name">
+                            <input class="inputForm border-2 focus:outline-none focus:shadow-outline" type="text" name="first_name" id="first_name" :value="old('first_name')" required autocomplete="first_name">
                             @error('first_name')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
                     </div>
-                    <div class="xl:flex xl:justify-between mb-4">
-                        <div class="flex flex-col">
+                    <div class="lg:flex lg:justify-between">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="Peudo">Mon pseudo *</label>
                             <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="text" name="pseudo" id="pseudo" :value="old('pseudo')" required autocomplete="pseudo">
                             @error('pseudo')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="flex flex-col">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="date_of_birth">Ma date de naissance</label>
                             <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="date" name="date_of_birth" id="date_of_birth" :value="old('date_of_birth')" autocomplete="date_of_birth">
                             @error('date_of_birth')
@@ -40,15 +41,15 @@
                             @enderror
                         </div>
                     </div>
-                    <div class="xl:flex xl:justify-between mb-4">
-                        <div class="flex flex-col">
+                    <div class="lg:flex lg:justify-between">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="email">Mon email *</label>
                             <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="email" name="email" id="email" :value="old('email')" required autocomplete="email">
                             @error('email')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="flex flex-col">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <x-clubselect />
                             @error('prefer_team')
                             <div class="alert alert-danger">{{ $message }}</div>
@@ -67,15 +68,15 @@
                     <div class="alert alert-danger">{{ $message }}</div>
                     @enderror
                 </div> -->
-                    <div class="xl:flex xl:justify-between mb-4">
-                        <div class="flex flex-col xl:w-5/12">
+                    <div class="lg:flex lg:justify-between mb-4">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="password">Mon mot de passe *</label>
                             <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="password" name="password" required autocomplete="new-password">
                             @error('password')
                             <div class="alert alert-danger">{{ $message }}</div>
                             @enderror
                         </div>
-                        <div class="flex flex-col xl:w-5/12">
+                        <div class="flex flex-col lg:w-5/12 mb-6">
                             <label for="passwordConfirm">Mot de passe à confirmer</label>
                             <input class="inputForm border-2 focus:outline-none focus:shadow-outline w-full my-1" type="password" name="password_confirmation" required autocomplete="new-password">
                         </div>
--- resources/views/footer.blade.php
+++ resources/views/footer.blade.php
@@ -1,4 +1,4 @@
-<footer class="bg-gray-900 h-72 mt-2">
+<footer class="bg-gray-900 h-72">
     <div class="flex flex-col justify-around text-white">
         <div class="flex justify-center p-2">
             <a class="px-4" href="http://" target="_blank"><img src="{{ asset('images/logoFacebook.png') }}" alt="logo Facebook" class="w-10"></a>
