2fded244252fa5b08b9e13e6adab05e47fe11115
Anthony <systaim@gmail.com>
2021-02-06 08:57:55 +0100

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -30,24 +30,28 @@
         </div>
     </div>
     <div class="flex flex-wrap justify-around items-center m-auto lg:px xl:w-8/12">
-        <div id="live" class="relative w-80 lg:w-96 bg-primary rounded-lg h-64 lg:h-72 p-2 text-white flex flex-col justify-around items-center m-6 shadow-xl">
-            <p class="font-bold">Les matchs en live</p>
-            <i class="relative text-6xl fas fa-microphone-alt my-4">
-                <div class="animate-ping absolute -top-0.5 right-1 bg-red-500 h-3 w-3 rounded-full z-10"></div>
-            </i>
-            <p class="absolute top-2 left-2 py-1 px-2 rounded-md text-xl text-primary font-bold bg-secondary">LIVE</p>
-            <div class="flex justify-end w-full">
-                <a href="/live" class="btn btnSecondary">Par ici !</a>
+        <a href="/live" class="cursor-pointer">
+            <div id="live" class="relative w-80 lg:w-96 bg-primary rounded-lg h-64 lg:h-72 p-2 text-white flex flex-col justify-around items-center m-6 shadow-xl">
+                <p class="font-bold uppercase">Les matchs en live</p>
+                <i class="relative text-6xl fas fa-microphone-alt my-4">
+                    <div class="animate-ping absolute -top-0.5 right-1 bg-red-500 h-3 w-3 rounded-full z-10"></div>
+                </i>
+                <p class="absolute top-2 left-2 py-1 px-2 rounded-md text-xl text-primary font-bold bg-secondary">LIVE</p>
+                <div class="flex justify-end w-full">
+                    <button class="btn btnSecondary">Par ici !</button>
+                </div>
+                <!-- <p>Tous les matchs en live</p> -->
             </div>
-            <!-- <p>Tous les matchs en live</p> -->
-        </div>
-        <div id="weekend" class="relative w-80 lg:w-96 bg-primary rounded-lg h-64 lg:h-72 px-2 text-white flex flex-col justify-between items-center m-6 shadow-xl py-4">
-            <p class="font-bold">Les matchs du week-end</p>
-            <!-- <i class="relative text-6xl fas fa-futbol my-4"></i> -->
-            <div class="flex justify-end w-full">
-                <a href="/matchesduweekend" class="btn btnSecondary">Je jette un oeil</a>
+        </a>
+        <a href="/matchsduweekend" class="cursor-ponter">
+            <div id="weekend" class="relative w-80 lg:w-96 bg-primary rounded-lg h-64 lg:h-72 px-2 text-white flex flex-col justify-between items-center m-6 shadow-xl py-4">
+                <p class="font-bold uppercase">Les matchs du week-end</p>
+                <!-- <i class="relative text-6xl fas fa-futbol my-4"></i> -->
+                <div class="flex justify-end w-full">
+                <button class="btn btnSecondary">Je jette un oeil</button>
+                </div>
             </div>
-        </div>
+        </a>
     </div>
     <div class="py-6">
         <div class="flex justify-center">
