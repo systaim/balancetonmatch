53827c67e79095cb7311406ef9b89be198579a81
Anthony <systaim@gmail.com>
2021-02-23 20:36:08 +0100

    Update clubs.sql

--- database/seeders/clubs.sql
+++ database/seeders/clubs.sql
@@ -41,7 +41,7 @@ CREATE TABLE `clubs` (
   `updated_at` timestamp NULL DEFAULT NULL,
   `deleted_at` timestamp NULL DEFAULT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=20851 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+) ENGINE=InnoDB AUTO_INCREMENT=20851 DEFAULT CHARSET=utf8 COLLATE=utf8_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
