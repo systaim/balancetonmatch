d89e638bbf13dfdb92c22d67d15a63ad5a275ee5
Anthony <systaim@gmail.com>
2021-03-04 11:21:42 +0100

    Update create.blade.php

--- resources/views/matches/create.blade.php
+++ resources/views/matches/create.blade.php
@@ -2,6 +2,13 @@
 @section('content')
 <section>
 
-    @livewire('create-match')
+    @livewire('create-match', [
+    'regions' => $regions->filter(),
+    'groups' => $groups->filter(),
+    'departments' => $departments->filter(),
+    'divisionsDepartments' => $divisionsDepartments->filter(),
+    'divisionsRegions' => $divisionsRegions->filter(),
+    'competitions' => $competitions->filter()
+    ])
 </section>
 @endsection
