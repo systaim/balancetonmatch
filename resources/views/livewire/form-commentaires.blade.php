34dbc63789bcd18011d20ac172462e4e500d97fd
systAIM <40884280+systaim@users.noreply.github.com>
2021-05-08 10:04:57 +0200

    Update form-commentaires.blade.php

--- resources/views/livewire/form-commentaires.blade.php
+++ resources/views/livewire/form-commentaires.blade.php
@@ -796,6 +796,19 @@
                     </div>
                 </div>
             </div>
+            <div>
+                <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
+                <!-- vertical match -->
+                <ins class="adsbygoogle"
+                    style="display:block"
+                    data-ad-client="ca-pub-7237777700901740"
+                    data-ad-slot="2316864272"
+                    data-ad-format="auto"
+                    data-full-width-responsive="true"></ins>
+                <script>
+                    (adsbygoogle = window.adsbygoogle || []).push({});
+                </script>
+            </div>
         @endif
     </div>
     <!-- Fin affichage des commentaires -->
