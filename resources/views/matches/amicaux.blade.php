90801c7a895f884bf3f8304f3fea739668fc8fe4
systAIM <40884280+systaim@users.noreply.github.com>
2021-08-14 11:00:04 +0200

    Update amicaux.blade.php

--- resources/views/matches/amicaux.blade.php
+++ resources/views/matches/amicaux.blade.php
@@ -23,14 +23,14 @@
             class="flex items-center lg:w-3/5 mx-auto border p-10 border-primary sm:flex-row flex-col rounded-lg shadow-2xl">
             <div
                 class="sm:w-32 sm:h-32 h-20 w-20 sm:mr-10 inline-flex items-center justify-center rounded-full bg-secondary text-primary flex-shrink-0">
-                <i class="fas fa-search text-4xl"></i>
+                <i class="fas fa-plus text-4xl"></i>
             </div>
             <div class="flex-grow sm:text-left text-center mt-6 sm:mt-0">
                 <h2 class="text-gray-900 text-lg title-font font-medium mb-2 m-4">Créer un match</h2>
-                <p class="leading-relaxed text-base">Le match que tu recherches n'est pas créer ? N'hésite pas à le
-                    faire</p>
+                <p class="leading-relaxed text-base">Le match que tu recherches n'est pas dans la liste ? N'hésite pas à le
+                    créer !</p>
                 <a class="flex justify-end" href="{{ route('matches.create') }}">
-                    <button class="btn btnSecondary">Crée un match</button>
+                    <button class="btn btnSecondary">Je crée un match</button>
                 </a>
             </div>
         </div>
