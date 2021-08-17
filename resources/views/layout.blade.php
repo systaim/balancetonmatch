ff6455204b8b316e2cb987075055512bf8cc2f4d
systAIM <40884280+systaim@users.noreply.github.com>
2021-08-17 12:55:18 +0200

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -26,7 +26,7 @@
     <link rel="stylesheet" href="{{ mix('css/styles.css') }}?ver=1.01" />
     <script src="https://kit.fontawesome.com/c03c2336c3.js" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/gh/alpinejs/alpine@v2.8.0/dist/alpine.min.js" defer></script>
-    {{-- <script data-ad-client="ca-pub-7237777700901740" async
+    <script data-ad-client="ca-pub-7237777700901740" async
         src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js"></script>
 
     <!-- Global site tag (gtag.js) - Google Analytics -->
@@ -43,7 +43,7 @@
     </script>
     @if (request()->path() == 'contact')
         <script src="https://www.google.com/recaptcha/api.js" async defer></script>
-    @endif --}}
+    @endif
 </head>
 
 <body>
