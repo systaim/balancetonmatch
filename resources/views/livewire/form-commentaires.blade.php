aeff54aae14372a7cfb2af6be712755d53e01333
systAIM <40884280+systaim@users.noreply.github.com>
2021-08-17 08:36:12 +0200

    Update form-commentaires.blade.php

--- resources/views/livewire/form-commentaires.blade.php
+++ resources/views/livewire/form-commentaires.blade.php
@@ -73,7 +73,7 @@
                                 {{ $home_score }}
                             </p>
                             @auth
-                                @if(Auth::user()->role == "super-admin" || $match->commentateur)
+                                {{-- @if(Auth::user()->role == "super-admin" || $match->commentateur) --}}
                                     @if($match->commentateur && $match->commentateur->user_id == Auth::user()->id || ($match->live == "finDeMatch" && Auth::user()->role == "manager" && (Auth::user()->club_id == $match->homeClub->id || Auth::user()->club_id == $match->awayClub->id)))
                                     <div class="flex justify-evenly items-center mt-1 z-10">
                                         <button type="button" wire:click="decrementHomeScore" class="focus:outline-none">
@@ -84,7 +84,7 @@
                                         </button>
                                     </div>
                                     @endif
-                                @endif
+                                {{-- @endif --}}
                             @endauth
                         </div>
                         <div class="z-10">
