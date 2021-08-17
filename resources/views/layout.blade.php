a4b64d126c8cc39cea76c1a5c321bc34c9cafe24
systAIM <40884280+systaim@users.noreply.github.com>
2021-08-17 13:47:12 +0200

    Update layout.blade.php

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -100,7 +100,7 @@
         @include('footer')
     </div>
 
-    <script src="{{ mix('js/app.js') }}?ver=1.01"></script>
+    <script src="{{ mix('js/app.js') }}?ver=1.011"></script>
     @livewireScripts
 </body>
 
