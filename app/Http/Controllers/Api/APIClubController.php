780141147d37fb5cab86b62a46a7acdc41bdf113
systAIM <40884280+systaim@users.noreply.github.com>
2021-08-18 14:36:13 +0200

    Update APIClubController.php

--- app/Http/Controllers/Api/APIClubController.php
+++ app/Http/Controllers/Api/APIClubController.php
@@ -38,7 +38,7 @@ class APIClubController extends Controller
      */
     public function show(Club $club)
     {
-        return new ClubResource($club);
+        return new ClubResource($club->id);
     }
 
     /**
