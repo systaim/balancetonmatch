cd2149f50bddd94ba191ccaf6b30c7d991181dfb
systAIM <40884280+systaim@users.noreply.github.com>
2021-08-18 15:33:42 +0200

    update

--- app/Http/Resources/ClubResource.php
+++ app/Http/Resources/ClubResource.php
@@ -14,17 +14,16 @@ class ClubResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
-        // return [
-        //     'abbreviation' => $this->abbreviation,
-        //     'nom' => $this->name,
-        //     'background' => $this->bg_path,
-        //     'color1' => $this->primary_color,
-        //     'color2' => $this->secondary_color,
-        //     'nbrEquipes' => $this->number_teams,
-        //     'adress' => $this->adress,
-        //     'CP' => $this->zip_code,
-        //     'city' => $this->city,
-        // ];
+        return [
+            'abbreviation' => $this->abbreviation,
+            'name' => $this->name,
+            'bg_path' => $this->bg_path,
+            'color1' => $this->primary_color,
+            'color2' => $this->secondary_color,
+            'nbrEquipes' => $this->number_teams,
+            'adress' => $this->adress,
+            'CP' => $this->zip_code,
+            'city' => $this->city,
+        ];
     }
 }
