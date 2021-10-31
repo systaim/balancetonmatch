765fdb797c2e973d7126de368c12d1288abfc9b2
systAIM <systaim@gmail.com>
2021-10-31 11:39:24 +0100

    update page club

--- resources/views/clubs/pageClub.blade.php
+++ resources/views/clubs/pageClub.blade.php
@@ -10,6 +10,54 @@
             @livewire('favori-team', ['user' => $user, 'club' => $club,'nbrFavoris' => $nbrFavoris])
         </div>
     </div>
+    <!-- devenir referent-club -->
+    @auth
+        @if (Auth::user()->club && Auth::user()->role == 'guest' && $club->id == Auth::user()->club_id)
+            <div class="w-11/12 md:w-8/12 lg:w-6/12 m-auto">
+                <div class="flex items-center justify-around my-2 bg-primary p-4 text-white shadow-xl rounded-lg">
+                    <div>
+                        <p>Devenir un des référent du club</p>
+                        <p class="text-sm">Cela permettra de mettre à jour les infos du club.</p>
+                        <p class="text-sm">Comme les intiales, les couleurs ou encore le logo</p>
+                    </div>
+
+                    <form action="{{ route('contacts.becomeManager') }}" method="post" style="display: none;">
+                        @csrf
+                    </form>
+                    <a id="btnDemande" class="btn btnSecondary">Demander</a>
+                </div>
+
+                <div id="formDirigeant" class="hidden fixed z-50 inset-0 justify-center items-center"
+                    style="background-color: rgba(0,0,0,.5);">
+                    <div class="absolute top-10 right-10">
+                        <a href="" class="text-4xl text-secondary font-bold">X</a>
+                    </div>
+                    <div class="p-10 bg-white w-full sm:w-11/12 md:w-9/12 lg:w-6/12 rounded-lg shadow-xl">
+                        <form action="{{ route('contacts.becomeManager') }}" method="post">
+                            @csrf
+                            <h2 class="text-center mb-6">Es-tu sûr de vouloir faire la demande pour <span
+                                    class="font-bold">{{ Auth::user()->club->name }}</span> ?</h2>
+                            <p>Avant de valider, vérifie bien ton adresse mail pour bien recevoir nos messages</p>
+                            <p>On reviendra très vite vers toi !</p>
+                            <div class="flex justify-end">
+                                <a href="" class="btn mx-8">J'annule</a>
+                                <input type="submit" class="btn btnSuccess" value="Je valide" />
+                            </div>
+                        </form>
+                    </div>
+                </div>
+                <script>
+                    let formDirigeant = document.getElementById("formDirigeant");
+                    let btnDemande = document.getElementById("btnDemande");
+
+                    btnDemande.addEventListener("click", function() {
+                        formDirigeant.classList.add("flex");
+                        formDirigeant.classList.remove("hidden");
+                    });
+                </script>
+            </div>
+        @endif
+    @endauth
     <div
         class="bg-primary text-white shadow-lg relative my-2 flex flex-col p-4 w-full m-auto md:w-8/12 lg:w-6/12 rounded-lg">
         <h4 class="text-center text-6xl">{{ $club->abbreviation }}</h4>
@@ -77,120 +125,96 @@
             @endif
         </div>
     </div>
-    <!-- devenir referent-club -->
-    @auth
-        @if (Auth::user()->club && Auth::user()->role == 'guest' && $club->id == Auth::user()->club_id)
-            <div class="w-11/12 md:w-8/12 lg:w-6/12 m-auto">
-                <div class="flex items-center justify-around my-2 bg-primary p-4 text-white shadow-xl rounded-lg">
-                    <p>Je souhaite devenir un des référent du club</p>
-                    <form action="{{ route('contacts.becomeManager') }}" method="post" style="display: none;">
-                        @csrf
-                    </form>
-                    <a id="btnDemande" class="btn btnSecondary">Demander</a>
-                </div>
-
-                <div id="formDirigeant" class="hidden fixed z-50 inset-0 justify-center items-center"
-                    style="background-color: rgba(0,0,0,.5);">
-                    <div class="absolute top-10 right-10">
-                        <a href="" class="text-4xl text-secondary font-bold">X</a>
-                    </div>
-                    <div class="p-10 bg-white w-full sm:w-11/12 md:w-9/12 lg:w-6/12 rounded-lg shadow-xl">
-                        <form action="{{ route('contacts.becomeManager') }}" method="post">
-                            @csrf
-                            <h2 class="text-center mb-6">Es-tu sûr de vouloir faire la demande pour <span
-                                    class="font-bold">{{ Auth::user()->club->name }}</span> ?</h2>
-                            <p>Avant de valider, vérifie bien ton adresse mail pour bien recevoir nos messages</p>
-                            <p>On reviendra très vite vers toi !</p>
-                            <div class="flex justify-end">
-                                <a href="" class="btn mx-8">J'annule</a>
-                                <input type="submit" class="btn btnSuccess" value="Je valide"></input>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
-        @endif
-    @endauth
 
     <div class="sm:w-11/12 md:w-9/12 xl:w-7/12 mx-auto my-10">
         <h3 class="text-2xl text-center my-4 border-b-2 border-darkGray">Prochain(s) match(s)</h3>
         @if (collect($matchsCF)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">Coupe de France</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">Coupe
+                    de France</h4>
                 @foreach ($matchsCF as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsBZH)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">Coupe de Bretagne</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">Coupe
+                    de Bretagne</h4>
                 @foreach ($matchsBZH as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsCoupeDep)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">Coupe Ange Lemée</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">Coupe
+                    Ange Lemée</h4>
                 @foreach ($matchsCoupeDep as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsR1)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">R1</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">R1
+                </h4>
                 @foreach ($matchsR1 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsR2)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">R2</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">R2
+                </h4>
                 @foreach ($matchsR2 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsR3)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">R3</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">R3
+                </h4>
                 @foreach ($matchsR3 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsD1)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D1</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D1
+                </h4>
                 @foreach ($matchsD1 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsD2)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D2</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D2
+                </h4>
                 @foreach ($matchsD2 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsD3)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D3</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D3
+                </h4>
                 @foreach ($matchsD3 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
         @if (collect($matchsD4)->isNotEmpty())
             <div class="mt-6">
-                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D4</h4>
+                <h4 class="inline-block text-xl px-3 text-center bg-secondary text-primary rounded-md shadow-lg ml-2">D4
+                </h4>
                 @foreach ($matchsD4 as $match)
-                        @include('match')
+                    @include('match')
                 @endforeach
             </div>
         @endif
@@ -212,13 +236,3 @@
         @endif
     </div>
 @endsection
-
-<script>
-    let formDirigeant = document.getElementById("formDirigeant");
-    let btnDemande = document.getElementById("btnDemande");
-
-    btnDemande.addEventListener("click", function() {
-        formDirigeant.classList.add("flex");
-        formDirigeant.classList.remove("hidden");
-    });
-</script>
