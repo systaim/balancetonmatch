886b4f852a22c39a1a94c7f66b9fa30afdf3b48e
systAIM <systaim@gmail.com>
2021-12-26 10:00:16 +0100

    refactoriser 2

--- database/migrations/2021_11_22_215658_create_category_articles_table.php
+++ database/migrations/2021_11_22_215658_create_category_articles_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateCategoriesTable extends Migration
+class CreateCategoriesArticleTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,7 +13,7 @@ class CreateCategoriesTable extends Migration
      */
     public function up()
     {
-        Schema::create('categories', function (Blueprint $table) {
+        Schema::create('category_articles', function (Blueprint $table) {
             $table->id();
             $table->string('title')->unique();
             $table->string('slug')->unique();
@@ -27,6 +27,6 @@ class CreateCategoriesTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('categories');
+        Schema::dropIfExists('category_articles');
     }
 }
