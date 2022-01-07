9edfb52169d7dc0b70fc0149b670e1c8d9e64609
systAIM <systaim@gmail.com>
2022-01-07 09:24:48 +0100

    Update show.blade.php

--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -98,56 +98,61 @@
         <div class="loader"></div>
     </div>
 
-    <div id="annonce" class="fixed bottom-16 inset-x-0 pb-2 sm:pb-5 z-50">
-        <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
-            <div class="p-2 rounded-lg bg-orange-600 shadow-lg sm:p-3">
-                <div class="flex items-center justify-between flex-wrap">
-                    <div class="w-0 flex-1 flex items-center">
-                        <span class="flex p-2 rounded-lg bg-orange-700">
-                            <!-- Heroicon name: outline/speakerphone -->
-                            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
-                            </svg>
-                        </span>
-                        <p class="ml-3 font-medium text-white truncate">
-                            <span class="md:hidden">
-                                L'heure du match n'est peut être pas bonne
-                            </span>
-                            <span class="hidden md:inline">
-                                Live ! L'heure du match n'est peut être pas bonne
-                            </span>
-                        </p>
-                    </div>
-                    <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
-                        <a href="{{ route('matches.edit', ['match' => $match]) }}"
-                            class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-800 bg-white">
-                            Modifier l'heure
-                        </a>
-                    </div>
-                    <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
-                        <button type="button" onclick="closeWindow()"
-                            class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white">
-                            <span class="sr-only">Dismiss</span>
-                            <!-- Heroicon name: outline/x -->
-                            <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                    d="M6 18L18 6M6 6l12 12" />
-                            </svg>
-                        </button>
+    @auth
+        @if ($match->live == 'attente')
+            <div id="annonce" class="fixed bottom-16 inset-x-0 pb-2 sm:pb-5 z-50">
+                <div class="max-w-7xl mx-auto px-2 sm:px-6 lg:px-8">
+                    <div class="p-2 rounded-lg bg-orange-600 shadow-lg sm:p-3">
+                        <div class="flex items-center justify-between flex-wrap">
+                            <div class="w-0 flex-1 flex items-center">
+                                <span class="flex p-2 rounded-lg bg-orange-700">
+                                    <!-- Heroicon name: outline/speakerphone -->
+                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
+                                    </svg>
+                                </span>
+                                <p class="ml-3 font-medium text-white truncate">
+                                    <span class="md:hidden">
+                                        L'heure du match n'est peut être pas bonne
+                                    </span>
+                                    <span class="hidden md:inline">
+                                        Live ! L'heure du match n'est peut être pas bonne
+                                    </span>
+                                </p>
+                            </div>
+                            <div class="order-3 mt-2 flex-shrink-0 w-full sm:order-2 sm:mt-0 sm:w-auto">
+                                <a href="{{ route('matches.edit', ['match' => $match]) }}"
+                                    class="flex items-center justify-center px-4 py-2 border border-transparent rounded-md shadow-sm text-sm font-medium text-blue-800 bg-white">
+                                    Modifier l'heure
+                                </a>
+                            </div>
+                            <div class="order-2 flex-shrink-0 sm:order-3 sm:ml-2">
+                                <button type="button" onclick="closeWindow()"
+                                    class="-mr-1 flex p-2 rounded-md hover:bg-indigo-500 focus:outline-none focus:ring-2 focus:ring-white">
+                                    <span class="sr-only">Dismiss</span>
+                                    <!-- Heroicon name: outline/x -->
+                                    <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                                        viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
+                                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                            d="M6 18L18 6M6 6l12 12" />
+                                    </svg>
+                                </button>
+                            </div>
+                        </div>
                     </div>
                 </div>
+                <script>
+                    function closeWindow() {
+                        let annonce = document.getElementById('annonce')
+                        annonce.style.display = "none"
+                    }
+                </script>
             </div>
-        </div>
-        <script>
-            function closeWindow() {
-                let annonce = document.getElementById('annonce')
-                annonce.style.display = "none"
-            }
-        </script>
-    </div>
+        @endif
+    @endauth
+
 
     <div id="container">
         <header class="relative top-O right-0 left-0 bg-white xl:h-auto z-50 lg:mt-0">
