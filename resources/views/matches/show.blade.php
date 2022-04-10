f70e427b7ae07f440e4575c9b541076ac9f5f750
systAIM <systaim@gmail.com>
2022-04-10 15:19:08 +0200

    Update show.blade.php

--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -291,7 +291,7 @@
             </svg>
         </div>
     </a> --}}
-    <div class="fixed bottom-0 mx-auto flex justify-center left-0 right-0 bg-primary">
+    <div class="fixed bottom-0 mx-auto flex justify-center left-0 right-0 bg-gray-100">
         <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7237777700901740"
                 crossorigin="anonymous"></script>
         <!-- bas de page -->
