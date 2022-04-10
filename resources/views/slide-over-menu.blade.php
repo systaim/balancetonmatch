1b1beb9df7579c9ec18ec0ede743faba584b8b20
systAIM <systaim@gmail.com>
2022-04-10 11:24:44 +0200

    Update slide-over-menu.blade.php

--- resources/views/slide-over-menu.blade.php
+++ resources/views/slide-over-menu.blade.php
@@ -49,7 +49,8 @@
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M13 5l7 7-7 7M5 5l7 7-7 7" />
                                 </svg>
-                                <a href="{{ route('home') }}">
+                                {{-- <a href="{{ route('home') }}"> --}}
+                                <a href="/">
                                     Accueil
                                 </a>
                             </li>
