653acdd6ba4a2120a9451d315a354879bdd1bb61
systAIM <systaim@gmail.com>
2022-04-10 11:26:19 +0200

    Update slide-over-menu.blade.php

--- resources/views/slide-over-menu.blade.php
+++ resources/views/slide-over-menu.blade.php
@@ -80,7 +80,8 @@
                                     <path stroke-linecap="round" stroke-linejoin="round"
                                         d="M13 5l7 7-7 7M5 5l7 7-7 7" />
                                 </svg>
-                                <a href="{{ route('contact') }}">
+                                {{-- <a href="{{ route('contact') }}"> --}}
+                                <a href="/contact">
                                     Contact
                                 </a>
                             </li>
