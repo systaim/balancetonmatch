5f07729eb6b3fe6a1347e39caf0b4eeb0328b27e
systAIM <systaim@gmail.com>
2022-07-02 18:54:41 +0200

    Update logo.blade.php

--- resources/views/clubs/logo.blade.php
+++ resources/views/clubs/logo.blade.php
@@ -2,13 +2,10 @@
     <div class="relative flex flex-col items-center justify-center mb-4 md:flex-row">
         <div class="m-4 flex w-28 h-28 rounded-full overflow-hidden shadow-xl">
             <div class="w-1/2" style="background-color:{{ $club->primary_color }}"></div>
-            <div class="flex items-center" style="background:linear-gradient(90deg, {{ $club->primary_color }}, {{ $club->secondary_color }})">
+            <div class="flex items-center"
+                style="background:linear-gradient(90deg, {{ $club->primary_color }}, {{ $club->secondary_color }})">
                 <div class="flex-grow-0 logo h-24 w-24">
-                    @if ($club->logo_path)
-                        <img class="object-contain" src="{{ asset($club->logo_path)}}" alt="Logo de {{ $club->name }}">
-                    @else
-                        <img class="object-contain" src="https://android-apiapp.azureedge.net/common/bib_img/logo/{{$club->numAffiliation}}.jpg" alt="logo">
-                    @endif
+                    <img class="object-contain" src="{{ asset($club->logo) }}" alt="Logo de {{ $club->name }}">
                 </div>
             </div>
             <div class="w-1/2" style="background-color:{{ $club->secondary_color }}"></div>
