a1154cee91acc75df56df536bc865b11b5e2b920
systAIM <systaim@gmail.com>
2022-09-22 08:21:59 +0200

    Update RecuperationMatchs.php

--- app/Http/Livewire/RecuperationMatchs.php
+++ app/Http/Livewire/RecuperationMatchs.php
@@ -15,7 +15,6 @@ class RecuperationMatchs extends Component
     public function mount($page)
     {
         $this->page = $page;
-        dd($this->competition_id);
     }
 
     public function recuperation()
@@ -24,8 +23,9 @@ class RecuperationMatchs extends Component
         $re = '/"date">\X+?(\d+) (\S+) (\d+) - (\d+)H(\d+)\X+?"equipe1">\X+?BC(\d{6})\X+?phlogos\/BC(\d{6})\X+?/m';
         if ($this->competition_id == 2) {
             $url = 'https://balancetonmatch.com/recup-matchs/D' . $this->division->id . '-' . $this->groupe->id . '-' . $this->departement->id . '.html';
+        } elseif ($this->competition_id == 1) {
+            $url = 'https://balancetonmatch.com/recup-matchs/R' . $this->division->id . '-' . $this->groupe->id . '.html';
         }
-        dd($url);
         $str = file_get_contents($url);
         preg_match_all($re, $str, $matches, PREG_SET_ORDER, 0);
         if (count($matches) == 182) {
