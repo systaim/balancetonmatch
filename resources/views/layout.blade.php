570b570f40f37d4788e0c0e24e36b9cf5ede23ca
systAIM <systaim@gmail.com>
2022-09-23 15:48:05 +0200

    update

--- resources/views/layout.blade.php
+++ resources/views/layout.blade.php
@@ -114,8 +114,8 @@
                     <a href=" /notifications">
                         <div
                             class="relative flex justify-center items-center text-primary border rounded-full h-12 w-12 mr-1">
-                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none"
-                                viewBox="0 0 24 24" stroke="currentColor">
+                            <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4" fill="none" viewBox="0 0 24 24"
+                                stroke="currentColor">
                                 <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                     d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" />
                             </svg>
@@ -162,6 +162,28 @@
     </div>
 </div>
 
+<div id="loaderPage" class="fixed inset-0 bg-primary bg-opacity-50 z-9999 flex justify-center items-center">
+    <div class="flex justify-center items-baseline">
+        <img src="{{ asset('images/favoris-mobile.png') }}" alt="" class="h-48">
+        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
+            stroke="currentColor" class="w-14 h-14 text-primary animate-spin">
+            <path stroke-linecap="round" stroke-linejoin="round"
+                d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0013.803-3.7M4.031 9.865a8.25 8.25 0 0113.803-3.7l3.181 3.182m0-4.991v4.99" />
+        </svg>
+    </div>
+</div>
+<script>
+    document.onreadystatechange = function() {
+        if (document.readyState != "complete") {
+            document.querySelector("body").style.visibility = "hidden";
+            document.querySelector("#loaderPage").style.visibility = "visible";
+        } else {
+            document.querySelector("#loaderPage").style.display = "none";
+            document.querySelector("body").style.visibility = "visible";
+        }
+    }
+</script>
+
 @if (request()->path() != '/')
     <a href=javascript:history.go(-1)>
         <div
@@ -175,15 +197,15 @@
     </a>
 @endif
 <div class="fixed bottom-0 mx-auto flex justify-center left-0 right-0 bg-gray-100">
-        <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7237777700901740"
-                crossorigin="anonymous"></script>
-        <!-- bas de page -->
-        <ins class="adsbygoogle" style="display:inline-block;width:90%;height:65px"
-            data-ad-client="ca-pub-7237777700901740" data-ad-slot="8168194089"></ins>
-        <script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </script>
-    </div>
+    <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7237777700901740"
+        crossorigin="anonymous"></script>
+    <!-- bas de page -->
+    <ins class="adsbygoogle" style="display:inline-block;width:90%;height:65px"
+        data-ad-client="ca-pub-7237777700901740" data-ad-slot="8168194089"></ins>
+    <script>
+        (adsbygoogle = window.adsbygoogle || []).push({});
+    </script>
+</div>
 @livewireScripts
 
 @auth
--- tailwind.config.js
+++ tailwind.config.js
@@ -183,6 +183,9 @@ module.exports = {
                 "white-xl":
                     "0 20px 25px -5px rgba(255, 255, 255, 0.1), 0 10px 10px -5px rgba(255, 255, 255, 0.04)",
             },
+            zIndex: {
+                '9999': '9999'
+            },
         },
     },
 };
