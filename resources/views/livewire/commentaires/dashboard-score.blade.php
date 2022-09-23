e8450903b8caae4233e09fae10325ffd78f1068d
systAIM <systaim@gmail.com>
2022-09-23 11:17:55 +0200

    Update dashboard-score.blade.php

--- resources/views/livewire/commentaires/dashboard-score.blade.php
+++ resources/views/livewire/commentaires/dashboard-score.blade.php
@@ -2,7 +2,7 @@
     <div class="">
         <div>
             <div wire:offline>
-                <div class="bg-orange-600 text-white py-2 flex justify-center ">
+                <div class="bg-orange-600 text-white py-2 flex justify-center w-full">
                     <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
                         stroke="currentColor" class="w-6 h-6 mr-2">
                         <path stroke-linecap="round" stroke-linejoin="round"
