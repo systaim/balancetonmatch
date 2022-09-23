256770c8d7bb7b0722def656e01165f6b9a010df
systAIM <systaim@gmail.com>
2022-09-23 17:41:54 +0200

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,12 +6,13 @@
             {{-- <img class="w-full h-full object-cover" src="{{ asset('images/home-preview.jpg') }}" alt="" loading="lazy"> --}}
             <video autoplay muted loop playsinline id="myVideo" class="w-full h-full object-cover">
                 <source src="{{ asset('videos/bg-search.mp4') }}" type="video/mp4">
-              </video>
+            </video>
             <div class="absolute inset-0 bg-gray-800 mix-blend-multiply" aria-hidden="true"></div>
         </div>
         <div class="relative max-w-7xl mx-auto px-4 sm:py-32 sm:px-6 lg:px-8">
             <h1 class="text-4xl font-extrabold tracking-tight text-white sm:text-5xl lg:text-6xl">Bienvenue</h1>
-            <p class="mt-6 text-sm lg:text-xl text-indigo-100 max-w-3xl">BalanceTonMatch.com a pour but de rassembler les passionnés du
+            <p class="mt-6 text-sm lg:text-xl text-indigo-100 max-w-3xl">BalanceTonMatch.com a pour but de rassembler les
+                passionnés du
                 ballon rond AMATEUR. Vous pourrez suivre les matchs en <span
                     class="uppercase text-primary font-bold bg-secondary px-2 rounded-sm">live</span> soit en tant que
                 commentateur soit en tant que spectateur</p>
@@ -57,26 +58,33 @@
             </div>
         </div>
     </div>
-    <!-- This example requires Tailwind CSS v2.0+ -->
+    <div class="p-3 relative overflow-hidden" style='background-image: url("{{ asset('images/terrain.jpg') }}"); background-size:cover'>
+        <div class="z-10">
+            <h3 class="text-center">Rechercher un club</h3>
+            <form action="/clubs" method="GET">
+                <div class="flex flex-col justify-center items-center">
+                    <input type="search" name="search" id="search" class="border rounded-md shadow-lg">
+                    <button type="submit" class="btn btnPrimary">Va chercher</button>
+                </div>
+            </form>
+        </div>
+    </div>
     <div class="bg-secondary text-primary">
         <div class="max-w-7xl mx-auto py-3 px-3 sm:px-6 lg:px-8">
             <div class="flex items-center justify-between flex-wrap">
-                <div class="w-0 flex-1 flex items-center">
+                <div class="flex justify-center items-center w-full">
                     <span class="flex p-2 rounded-lg bg-primary">
                         <!-- Heroicon name: outline/speakerphone -->
-                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
-                            stroke="currentColor" aria-hidden="true">
+                        <svg class="h-6 w-6 text-white" xmlns="http://www.w3.org/2000/svg" fill="none"
+                            viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
                             <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
                                 d="M11 5.882V19.24a1.76 1.76 0 01-3.417.592l-2.147-6.15M18 13a3 3 0 100-6M5.436 13.683A4.001 4.001 0 017 6h1.832c4.1 0 7.625-1.234 9.168-3v14c-1.543-1.766-5.067-3-9.168-3H7a3.988 3.988 0 01-1.564-.317z" />
                         </svg>
                     </span>
                     <p class="ml-3 font-medium">
-                        <span class="md:inline">
-                            Créez vos matchs amicaux pour pouvoir les commenter en direct
-                        </span>
+                        Créez vos matchs amicaux pour pouvoir les commenter en direct
                     </p>
                 </div>
-                
             </div>
             <div class="flex justify-center">
                 <a href="{{ route('matches.create') }}">
@@ -160,6 +168,7 @@
                                         </a>
                                     @endif
                                 @break
+
                                 @case('create_match')
                                     @if ($activite->match)
                                         <a href="{{ route('matches.show', [$activite->match->id]) }}">
@@ -205,6 +214,7 @@
                                         </a>
                                     @endif
                                 @break
+
                                 @case('create_commentator')
                                     @if ($activite->match)
                                         <a href="{{ route('matches.show', [$activite->match->id]) }}">
@@ -244,6 +254,7 @@
                                         </a>
                                     @endif
                                 @break
+
                                 @case('update_cover')
                                     @if ($activite->club)
                                         <a href="{{ route('clubs.show', [$activite->club->id]) }}">
@@ -282,10 +293,10 @@
                                         </a>
                                     @endif
                                 @break
+
                                 @case('create_player')
                                     @if ($activite->player)
-                                        <a
-                                            href="{{ route('clubs.players.show', [$activite->club->id, $activite->player->id]) }}">
+                                        <a href="{{ route('clubs.players.show', [$activite->club->id, $activite->player->id]) }}">
                                             <li class="py-4 hover:bg-gray-50 px-4">
                                                 <div class="flex space-x-3 ">
                                                     <div class="flex-1 space-y-1 ">
@@ -322,8 +333,8 @@
                                         </a>
                                     @endif
                                 @break
-                                @default
 
+                                @default
                             @endswitch
                         @endforeach
                     </ul>
@@ -398,7 +409,6 @@
                                     {{ count($liveMatches) == 1 ? 'match' : 'matchs' }} en cours
                                 </p>
                             </div>
-
                         @endif
                         <p class="text-white text-lg font-medium flex items-center mt-2">
                             J'y vais
@@ -634,12 +644,11 @@
                 </div>
                 <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
                     @foreach ($all_articles->sortByDesc('updated_at') as $article)
-
                         <div class="flex flex-col rounded-lg shadow-lg overflow-hidden">
                             <a href="{{ route('articles.show', $article) }}">
                                 <div class="flex-shrink-0">
-                                    <img class="h-48 w-full object-cover" src="{{ asset($article->image) }}" alt=""
-                                        loading="lazy">
+                                    <img class="h-48 w-full object-cover" src="{{ asset($article->image) }}"
+                                        alt="" loading="lazy">
                                 </div>
                             </a>
                             <div class="flex-1 bg-white p-6 flex flex-col justify-between">
