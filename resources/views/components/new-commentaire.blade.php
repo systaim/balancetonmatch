f1106b2d252f502e95c7ed232bf2f341cc58048c
systAIM <systaim@gmail.com>
2022-09-26 09:56:37 +0200

    Update new-commentaire.blade.php

--- resources/views/components/new-commentaire.blade.php
+++ resources/views/components/new-commentaire.blade.php
@@ -5,8 +5,7 @@
             @auth
                 <div class="flex my-3 items-center">
                     <p>Le score est correct ?</p>
-                    <button class="border rounded-sm px-1 border-secondary ml-10"
-                        wire:click="validateScoreMatch">Oui</button>
+                    <button class="border rounded-sm px-1 border-secondary ml-10" wire:click="validateScoreMatch">Oui</button>
                 </div>
                 <p>Sinon tu peux renseigner les infos supplémentaires</p>
             @else
@@ -18,45 +17,51 @@
     </div>
 @endif
 <div class="flex items-center w-11/12 mx-auto my-1 {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }}">
-    <div class="flex {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }} items-center {{ $comment->team_action != 'match' ? 'border rounded-md shadow-lg px-2 py-1 my-1' : '' }}">
+    <div
+        class="flex {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }} items-center {{ $comment->team_action != 'match' ? 'border rounded-md shadow-lg px-2 py-1 my-1' : '' }}">
         @if ($comment->type_action != 'substitute' && $comment->team_action != 'match')
             <p class="text-sm font-bold {{ $comment->team_action == 'away' ? 'ml-2 text-right' : 'text-left mr-2' }}">
-                {{ $comment->minute }}'</p>
-            <img class="w-4 h-4 mx-1 text-center" src="{{ asset($comment->icon) }}" alt="">
-            <p class="text-sm {{ $comment->team_action == 'away' ? 'mr-2' : 'ml-2' }}">
-                {{ $comment->{$comment->type_action}->player->full_name }}</p>
-            @if ($comment->type_action == 'goal' && $comment->passeur_id)
+                {{ $comment->minute }}'
+            </p>
+            <div>
                 <div class="flex items-center">
-                    {{-- <img class="w-4 h-4 mx-1 text-center" src="{{ asset('images/passeD.png') }}" alt=""> --}}
-                    <p class="flex items-center text-xs {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }}">
-                        <svg xmlns="http://www.w3.org/2000/svg"
-                            class="h-3 w-3 {{ $comment->team_action == 'home' ? 'transform rotate-180 ml-2' : '' }}"
-                            viewBox="0 0 20 20" fill="currentColor">
-                            <path fill-rule="evenodd"
-                                d="M10.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L12.586 11H5a1 1 0 110-2h7.586l-2.293-2.293a1 1 0 010-1.414z"
-                                clip-rule="evenodd" />
-                        </svg>
-                        {{ $comment->passeur->player->full_name }}
+                    <img class="w-4 h-4 mx-2 text-center" src="{{ asset($comment->icon) }}" alt="">
+                    <p class="text-sm {{ $comment->team_action == 'away' ? 'mr-2' : 'ml-2' }}">
+                        {{ $comment->{$comment->type_action}->player->full_name }}
                     </p>
                 </div>
-            @endif
+                @if ($comment->type_action == 'goal' && $comment->passeur_id)
+                    <div class="flex items-center my-1">
+                        <img class="w-4 h-4 mx-2 text-center" src="{{ asset('images/passeD.png') }}" alt="">
+                        <p
+                            class="flex items-center text-xs {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }}">
+
+                            {{ $comment->passeur->player->full_name }}
+                        </p>
+                    </div>
+                @endif
+            </div>
+            <div class="flex items-center">
+                @foreach ($reactions as $reaction)
+                    <button type="button" class=" h-8 w-8 flex justify-center items-center">
+                        {{ $reaction->emoji }}
+                    </button>
+                @endforeach
+            </div>
             @if (isset($comment->comments))
                 <p class="text-xs {{ $comment->team_action == 'away' ? 'mr-2' : 'ml-2' }}">
                     {{ '(' . $comment->comments . ')' }}
                 </p>
             @endif
         @endif
-    </div>
-    <div
-        class="flex {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }} items-center">
-
         @if ($comment->type_action == 'substitute')
             <p class="text-sm font-bold {{ $comment->team_action == 'away' ? 'ml-2 text-right' : 'text-left mr-2' }}">
                 {{ $comment->minute }}'
             </p>
             <img class="w-4 h-4 text-center" src="{{ asset($comment->icon) }}" alt="">
             <div>
-                <div class="text-sm flex items-center {{ $comment->team_action == 'away' ? 'flex-row-reverse mr-2' : 'ml-2' }}">
+                <div
+                    class="text-sm flex items-center {{ $comment->team_action == 'away' ? 'flex-row-reverse mr-2' : 'ml-2' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-green-500" viewBox="0 0 20 20"
                         fill="currentColor">
                         <path fill-rule="evenodd"
@@ -65,7 +70,8 @@
                     </svg>
                     {{ $comment->in_substitute->player->full_name }}
                 </div>
-                <div class="text-sm flex items-center {{ $comment->team_action == 'away' ? 'flex-row-reverse mr-2' : 'ml-2' }}">
+                <div
+                    class="text-sm flex items-center {{ $comment->team_action == 'away' ? 'flex-row-reverse mr-2' : 'ml-2' }}">
                     <svg xmlns="http://www.w3.org/2000/svg" class="h-4 w-4 text-red-600" viewBox="0 0 20 20"
                         fill="currentColor">
                         <path fill-rule="evenodd"
@@ -76,18 +82,20 @@
                 </div>
             </div>
         @endif
+    </div>
+    <div class="flex {{ $comment->team_action == 'away' ? 'flex-row-reverse' : '' }} items-center">
+
         @if ($comment->commentator->user_id == Auth::id() && $comment->team_action != 'match')
-        <div class="{{ $comment->team_action == 'away' ? 'mr-4' : 'ml-4' }}">
-            <button type="button" wire:click="openDeleteComment({{ $comment->id }})">
-                <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 border border-primary rounded-full"
-                    viewBox="0 0 20 20" fill="currentColor">
-                    <path fill-rule="evenodd"
-                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                        clip-rule="evenodd" />
-                </svg>
-            </button>
-        </div>
-            
+            <div class="{{ $comment->team_action == 'away' ? 'mr-4' : 'ml-4' }}">
+                <button type="button" wire:click="openDeleteComment({{ $comment->id }})">
+                    <svg xmlns="http://www.w3.org/2000/svg" class="h-3 w-3 border border-primary rounded-full"
+                        viewBox="0 0 20 20" fill="currentColor">
+                        <path fill-rule="evenodd"
+                            d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                            clip-rule="evenodd" />
+                    </svg>
+                </button>
+            </div>
         @endif
     </div>
 </div>
