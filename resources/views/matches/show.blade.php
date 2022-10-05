cb8264d372019b5fa31edb11c80ea94dce5ed98c
systAIM <systaim@gmail.com>
2022-10-05 22:30:16 +0200

    Update show.blade.php

--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -289,9 +289,9 @@
         <div class="bg-secondary">
             <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-7237777700901740"
                 crossorigin="anonymous"></script>
-            <!-- mitemps1 -->
-            <ins class="adsbygoogle" style="display:inline-block;width:300px;height:80px"
-                data-ad-client="ca-pub-7237777700901740" data-ad-slot="1296607207"></ins>
+            <!-- chargement match -->
+            <ins class="adsbygoogle" style="display:block" data-ad-client="ca-pub-7237777700901740"
+                data-ad-slot="3065533188" data-ad-format="auto" data-full-width-responsive="true"></ins>
             <script>
                 (adsbygoogle = window.adsbygoogle || []).push({});
             </script>
