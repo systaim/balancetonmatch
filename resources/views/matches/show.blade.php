8701cad6a4a5dec8e8c6b52dc77be3a6e10da349
systAIM <systaim@gmail.com>
2022-10-06 07:41:28 +0200

    update

--- resources/views/matches/show.blade.php
+++ resources/views/matches/show.blade.php
@@ -2,7 +2,6 @@
 <html lang="fr">
 
 <head>
-    <meta name="ga-site-verification" content="yjuNBtXv2Ld8JNkTx15eFWqD" />
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="csrf-token" content="{{ csrf_token() }}">
